85f789145dcf0911b2ae55059d6599a5c049bd60
Łukasz Wacławski <lwaclaws@gmail.com>
2017-04-30 18:23:44 +0200

    Fix tokenizer

--- src/main/scala/classify/DocumentPreprocessor.scala
+++ src/main/scala/classify/DocumentPreprocessor.scala
@@ -1,14 +1,21 @@
 package classify
 
 import java.io.StringReader
-import scala.collection.JavaConverters._
 
+import edu.stanford.nlp.process.PTBTokenizer
+
+import scala.collection.JavaConverters._
 import edu.stanford.nlp.util.StringUtils
 
 
 class DocumentPreprocessor(val ngramOrder: Int) {
   def getTermsFromDocument(document: String): List[String] = {
-    val coreNlpPreprocessor = new edu.stanford.nlp.process.DocumentPreprocessor(new StringReader(document))
+    val coreNlpPreprocessor = new edu.stanford.nlp.process.DocumentPreprocessor(new StringReader(
+      document.replaceAll("[^\u0000-\uFFFF]", " ")) // replace all non-lingual characters with space
+    )
+    val factory = PTBTokenizer.factory()
+    factory.setOptions("untokenizable=noneDelete")
+    coreNlpPreprocessor.setTokenizerFactory(factory)
     for {
       sentence <- coreNlpPreprocessor.asScala.toList
       words = sentence.asScala.map(_.word()).toList
